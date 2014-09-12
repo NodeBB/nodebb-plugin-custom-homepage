be6080047eaaa4b64eccc05335d58a87db938b83
Nick Janssen <info@nickjanssen.com>
2014-09-12 18:12:33 +0200

    Ajaxify, fix nav link, use static:app.load, use correct template for widgets

--- library.js
+++ library.js
@@ -1,33 +1,48 @@
 (function(module) {
 	"use strict";
 
+	// express removal of routes
+	var unmount = function (app, path) {
+	    for (var i = 0, len = app.stack.length; i < len; ++i) {
+	        if (app.stack[i].route && app.stack[i].route.path == path) {
+	            app.stack.splice(i, 1);
+	            return true;
+	        };
+	    }
+	    return false;
+	}
+
 	var Plugin = {};
 
 	function renderHomepage(req, res, next) {
 		res.render('homepage', {});
 	}
 
-    Plugin.init = function(app, middleware, controllers) {
+    Plugin.init = function(app, middleware, controllers, callback) {
+    	// For some reason, the main / route cannot be overwritten without deleting it first
+    	unmount(app, '/');
+
     	app.get('/', middleware.buildHeader, renderHomepage);
 		app.get('/api/home', function(req, res, next) {
 			res.json({});
 		});
-		app.get('/templates/home.tpl', renderHomepage);
 
 		app.get('/forum', middleware.buildHeader, controllers.home);
 		app.get('/api/forum', controllers.home);
+
+    	callback();
     };
 
     Plugin.addNavigation = function(header, callback) {
-		header.navigation = header.navigation.concat(
-			[
-				{
-					route: '/forum',
-					class: '',
-					text: "<i class=\"fa fa-fw fa-comments\" title=\"Forum\"></i>",
-					title: 'Forum'
-				}
-			]
+		header.navigation.push(
+			{
+				route: '/forum',
+				class: '',
+				text: 'Forum',
+				iconClass: 'fa-comments',
+				title: 'Forum',
+				textClass: 'visible-xs-inline'
+			}
 		);
 
 		callback(false, header);
@@ -37,27 +52,34 @@
 		areas = areas.concat([
 			{
 				'name': 'Custom HP Header',
-				'template': 'home.tpl',
+				'template': 'homepage.tpl',
 				'location': 'hp-header'
 			},
 			{
 				'name': 'Custom HP Footer',
-				'template': 'home.tpl',
+				'template': 'homepage.tpl',
 				'location': 'hp-footer'
 			},
 			{
 				'name': 'Custom HP Sidebar',
-				'template': 'home.tpl',
+				'template': 'homepage.tpl',
 				'location': 'hp-sidebar'
 			},
 			{
 				'name': 'Custom HP Content',
-				'template': 'home.tpl',
+				'template': 'homepage.tpl',
 				'location': 'hp-content'
 			}
 		]);
 
 		callback(null, areas);
+	};
+
+	Plugin.changeClientRouting = function (config, callback) {
+		config.custom_mapping['^/?$'] = 'homepage';
+		config.custom_mapping['^forum?$'] = 'home';
+
+		callback(null, config);
 	};
 
 	module.exports = Plugin;
